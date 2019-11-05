ddd413b50a14e93e9e9ce2478ac0a98d55bc368a
Stephan Oudmaijer <soudmaijer@gmail.com>
2019-11-05 15:47:12 +0100

    Cleanup

--- src/main/java/com/bootique/bootique/BootiqueApplication.kt
+++ src/main/java/com/bootique/bootique/BootiqueApplication.kt
@@ -3,7 +3,6 @@ package com.bootique.bootique
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
@@ -23,16 +22,14 @@ class BootiqueApplication {
     fun api(): Docket {
         return Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.bootique.bootique"))
                 .build()
     }
 
+    /**
+     * Runs the Spring boot application.
+     */
     companion object {
-
-        /**
-         * Runs the Spring boot application.
-         */
         @JvmStatic
         fun main(args: Array<String>) {
             SpringApplication.run(BootiqueApplication::class.java, *args)
