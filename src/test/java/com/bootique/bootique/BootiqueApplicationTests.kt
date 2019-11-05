9413726ea8af28652bfb72a39b3e53a97a7c5ea9
Stephan Oudmaijer <soudmaijer@gmail.com>
2019-11-05 14:12:54 +0100

    Polish

--- src/test/java/com/bootique/bootique/BootiqueApplicationTests.kt
+++ src/test/java/com/bootique/bootique/BootiqueApplicationTests.kt
@@ -9,7 +9,9 @@ import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.exchange
 import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
@@ -21,14 +23,25 @@ class BootiqueApplicationTest {
 
     @Test
     fun `get products endpoint should return a list of products`() {
-        val products = testRestTemplate.exchange<List<Product>>("/products", HttpMethod.GET, HttpEntity.EMPTY).body!!
+        val response = testRestTemplate.exchange<List<Product>>("/products", HttpMethod.GET, HttpEntity.EMPTY)
+        assertThat(response.statusCode.value()).isEqualTo(200)
+
+        val products = response.body!!
         assertThat(products.size).isEqualTo(4)
         assertThat(products.first().title).isEqualTo("iPhone X")
     }
 
     @Test
     fun `add product to basket results in updated basket`() {
-        val httpEntity = HttpEntity(OrderItem(productId = "1", quantity = 2))
+        val productId = "1"
+        val quantity = 2
+        val body = """
+            {
+                "productId":"$productId",
+                "quantity":$quantity
+            }
+        """
+        val httpEntity = HttpEntity(body, HttpHeaders().apply { contentType = MediaType.APPLICATION_JSON })
         val response = testRestTemplate.postForEntity<Basket>("/baskets/1/items", httpEntity)
         assertThat(response.statusCode.value()).isEqualTo(200)
     }
