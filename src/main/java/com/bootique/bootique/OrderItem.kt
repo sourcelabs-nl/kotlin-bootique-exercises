1b3cc2928998675ef84fa964b25d80f5b62ea473
Stephan Oudmaijer <soudmaijer@gmail.com>
2019-11-05 15:43:55 +0100

    Cleanup

--- src/main/java/com/bootique/bootique/OrderItem.kt
+++ src/main/java/com/bootique/bootique/OrderItem.kt
@@ -6,5 +6,11 @@ import java.math.BigDecimal
  * Represents the product, quantity and price of an item in the Basket.
  */
 data class OrderItem(val productId: String, val quantity: Int, val price: BigDecimal = BigDecimal.ZERO) {
+
+    /**
+     * Calculated totalPrice of this item: price * quantity
+     *
+     * @return BigDecimal.ZERO if no price is defined
+     */
     val totalPrice = price * quantity
 }
