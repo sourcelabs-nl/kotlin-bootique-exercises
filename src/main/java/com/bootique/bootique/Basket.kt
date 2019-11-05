6ab0523660f6cf40b01ee6a5c93d796a0923574e
Stephan Oudmaijer <soudmaijer@gmail.com>
2019-11-05 15:45:08 +0100

    Cleanup

--- src/main/java/com/bootique/bootique/Basket.kt
+++ src/main/java/com/bootique/bootique/Basket.kt
@@ -7,6 +7,10 @@ import java.math.BigDecimal
  */
 class Basket(val orderItems: MutableList<OrderItem> = mutableListOf()) {
 
+    /**
+     * Calculated sum of the order item totalPrice.
+     * @return BigDecimal.ZERO in case of an empty basket.
+     */
     val totalPrice: BigDecimal
         get() = orderItems.sumBy { it.totalPrice }
 
