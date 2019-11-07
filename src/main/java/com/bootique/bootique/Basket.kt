cb00d1867810b50526320c2c24b47ece18351e04
Stephan Oudmaijer <soudmaijer@gmail.com>
2019-11-07 16:05:53 +0100

    Basket: orderItems should be items

--- src/main/java/com/bootique/bootique/Basket.kt
+++ src/main/java/com/bootique/bootique/Basket.kt
@@ -5,14 +5,14 @@ import java.math.BigDecimal
 /**
  * Basket contains the order items for a specific user or session.
  */
-class Basket(val orderItems: MutableList<OrderItem> = mutableListOf()) {
+class Basket(val items: MutableList<OrderItem> = mutableListOf()) {
 
     /**
      * Calculated sum of the order item totalPrice.
      * @return BigDecimal.ZERO in case of an empty basket.
      */
     val totalPrice: BigDecimal
-        get() = orderItems.sumBy { it.totalPrice }
+        get() = items.sumBy { it.totalPrice }
 
-    fun addOrderItem(orderItem: OrderItem) = orderItems.add(orderItem)
+    fun addOrderItem(orderItem: OrderItem) = items.add(orderItem)
 }
