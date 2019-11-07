6592a53b8ceec9fc5b2091f9e542f840f0872f2b
Stephan Oudmaijer <soudmaijer@gmail.com>
2019-11-07 16:04:38 +0100

    Basket: orderItems should be items

--- src/main/java/com/bootique/bootique/Basket.java
+++ src/main/java/com/bootique/bootique/Basket.java
@@ -1,18 +1,25 @@
 package com.bootique.bootique;
 
 import java.math.BigDecimal;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Basket contains the order items for a specific user or session.
  */
 public class Basket {
-    private final List<OrderItem> items = new CopyOnWriteArrayList<>();
+    private final List<OrderItem> items;
 
-    public List<OrderItem> getOrderItems() {
-        return Collections.unmodifiableList(items);
+    public Basket() {
+        items = new ArrayList<>();
+    }
+
+    public Basket(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
     }
 
     public void addOrderItem(OrderItem orderItem) {
@@ -20,7 +27,8 @@ public class Basket {
     }
 
     /**
-     * Calculates the sum of the order item totalPrice.
+     * Calculated sum of the order item totalPrice.
+     *
      * @return BigDecimal.ZERO in case of an empty basket.
      */
     public BigDecimal getTotalPrice() {
