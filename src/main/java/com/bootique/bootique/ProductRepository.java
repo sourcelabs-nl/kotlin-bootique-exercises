bd9629274b1f94fff6aa927bc5fe934dea8e49d5
Stephan Oudmaijer <soudmaijer@gmail.com>
2021-05-11 20:24:47 +0200

    Update Java version to newer Java 10/11 collection usages.

--- src/main/java/com/bootique/bootique/ProductRepository.java
+++ src/main/java/com/bootique/bootique/ProductRepository.java
@@ -3,11 +3,8 @@ package com.bootique.bootique;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Dummy implementation of a Product persistent store, keeps the products in memory.
@@ -17,19 +14,23 @@ import java.util.concurrent.ConcurrentHashMap;
 @Repository
 public class ProductRepository {
 
-    private static final Map<String, Product> products = new ConcurrentHashMap<>();
+    private static final Map<String, Product> products = Map.of(
+        "1", new Product("1", "iPhone XX", "Apple", new BigDecimal("3989.99")),
+        "2", new Product("2", "Galaxy S25", "Samsung", new BigDecimal("2699.99")),
+        "3", new Product("3", "3310", "Nokia", new BigDecimal("19.95")),
+        "4", new Product("4", "Kermit", "KPN", new BigDecimal("6.95"))
+    );
 
-    static {
-        products.put("1", new Product("1", "iPhone X", "Apple", new BigDecimal("989.99")));
-        products.put("2", new Product("2", "Galaxy S8", "Samsung", new BigDecimal("699.99")));
-        products.put("3", new Product("3", "3310", "Nokia", new BigDecimal("19.95")));
-        products.put("4", new Product("4", "Kermit", "KPN", new BigDecimal("6.95")));
-    }
-
+    /**
+     * @return an unmodifiable list containing products
+     */
     public List<Product> getProducts() {
-        return Collections.unmodifiableList(new ArrayList<>(products.values()));
+        return List.copyOf(products.values());
     }
 
+    /**
+     * @return null if not found
+     */
     public Product getProductById(String productId) {
         return products.get(productId);
     }
