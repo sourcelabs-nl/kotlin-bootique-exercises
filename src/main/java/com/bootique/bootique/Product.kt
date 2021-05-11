ad27f33066907b7fdec5ca627dc889302a7db777
Stephan Oudmaijer <soudmaijer@gmail.com>
2021-05-11 22:28:00 +0200

    does not need to be a data class.

--- src/main/java/com/bootique/bootique/Product.kt
+++ src/main/java/com/bootique/bootique/Product.kt
@@ -5,4 +5,4 @@ import java.math.BigDecimal
 /**
  * A product that can be bought.
  */
-data class Product(val id: String, val title: String, val brand: String, val listPrice: BigDecimal)
+class Product(val id: String, val title: String, val brand: String, val listPrice: BigDecimal)
